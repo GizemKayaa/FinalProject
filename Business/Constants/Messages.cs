af7a78601a0e3eee0c1591fd6d922f29b18a2713
GizemKayaa <giizeeem61@gmail.com>
2021-02-24 02:13:07 +0200

    project

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "added";
+        public static string ProductNameInvalid = "product name is invalid";
+        public static string MaintenanceTime = "sistem bakımda";
+        public static string ProductsListed = "products listed";
+    }
+}
