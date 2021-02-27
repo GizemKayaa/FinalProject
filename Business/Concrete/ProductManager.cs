a515ae8924e642db9137680e2ab2377ed521af17
GizemKayaa <giizeeem61@gmail.com>
2021-02-28 01:56:58 +0200

    project

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Business.BusinessAspects.Autofac;
 using Business.CCS;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
@@ -30,7 +31,8 @@ namespace Business.Concrete
 
         }
         
-
+        //claim
+        [SecuredOperation("product.add,admin")]
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
